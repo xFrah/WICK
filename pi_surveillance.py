67a7dd8b26d7ce998dbcf3fd7ee71e52037f95ed
xFrah <fdimonaco309@hotmail.com>
2022-10-04 19:23:45 +0200

    input

--- pi_surveillance.py
+++ pi_surveillance.py
@@ -62,56 +62,60 @@ while True:
     # text
     original_frame = vs.read()
     # timestamp = datetime.datetime.now()
-    text = "Unoccupied"
+#    text = "Unoccupied"
 
     # resize the frame, convert it to grayscale, and blur it
     frame = imutils.resize(original_frame, width=500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-    # if the average frame is None, initialize it
-    if avg is None:
-        print("[INFO] starting background model...")
-        avg = gray.copy().astype("float")
+    if input() != "q":
         continue
 
-    # accumulate the weighted average between the current frame and
-    # previous frames, then compute the difference between the current
-    # frame and running average
-    cv2.accumulateWeighted(gray, avg, 0.5)
-    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-
-    # threshold the delta image, dilate the thresholded image to fill
-    # in holes, then find contours on thresholded image
-    thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
+#    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+#
+#    # if the average frame is None, initialize it
+#    if avg is None:
+#        print("[INFO] starting background model...")
+#        avg = gray.copy().astype("float")
+#        continue
+#
+#    # accumulate the weighted average between the current frame and
+#    # previous frames, then compute the difference between the current
+#    # frame and running average
+#    cv2.accumulateWeighted(gray, avg, 0.5)
+#    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+#
+#    # threshold the delta image, dilate the thresholded image to fill
+#    # in holes, then find contours on thresholded image
+#    thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
+#    thresh = cv2.dilate(thresh, None, iterations=2)
+#    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#    cnts = imutils.grab_contours(cnts)
 
     # loop over the contours
-    for c in cnts:
-        # if the contour is too small, ignore it
-        if cv2.contourArea(c) < conf["min_area"]:
-            continue
-
-        #movement_detected = True
-        #last_movement = datetime.datetime.now()
-
-        # compute the bounding box for the contour, draw it on the frame,
-        # and update the text
-        (x, y, w, h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        text = "Occupied"
-
-    cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+#    for c in cnts:
+#        # if the contour is too small, ignore it
+#        if cv2.contourArea(c) < conf["min_area"]:
+#            continue
+#
+#        #movement_detected = True
+#        #last_movement = datetime.datetime.now()
+#
+#        # compute the bounding box for the contour, draw it on the frame,
+#        # and update the text
+#        (x, y, w, h) = cv2.boundingRect(c)
+#        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+#        text = "Occupied"
+#
+#    cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
     # check to see if the frames should be displayed to screen
     if conf["show_video"]:
         # display the security feed
         # show the frame and record if the user presses a key
 
-        cv2.imshow("Thresh", thresh)
-        cv2.imshow("Frame Delta", frameDelta)
+#        cv2.imshow("Thresh", thresh)
+#        cv2.imshow("Frame Delta", frameDelta)
         cv2.imshow("Image", frame)
         key = cv2.waitKey(1) & 0xFF
 
