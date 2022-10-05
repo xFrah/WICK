d32e5a2e59e248e454d1957fbe773a4859bb2c9d
xFrah <fdimonaco309@hotmail.com>
2022-10-05 16:39:08 +0200

    added servos

--- pi_surveillance.py
+++ pi_surveillance.py
@@ -12,6 +12,11 @@ from PIL import Image
 from datetime import datetime
 from uuid import uuid4
 import serial
+import Servo
+
+myservo = []
+closed_position = 0
+open_position = 180
 
 
 def normalize(height, maximum, n):
@@ -38,16 +43,26 @@ def parse_serial(stringa):
         return None
 
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-c", "--conf", required=True,
-                help="path to the JSON configuration file")
-args = vars(ap.parse_args())
+def setup_servos():
+    for i in range(0, 16):
+        myservo.append(Servo.Servo(i))
+        Servo.Servo(i).setup()
+
+
+def open_thing(servo_number):
+    for i in range(closed_position, open_position, 5):
+        myservo[servo_number].write(i)
+        time.sleep(0.1)
+    time.sleep(2)
+    for i in range(closed_position, open_position, 5)[::-1]:
+        myservo[servo_number].write(i)
+        time.sleep(0.1)
+
 
 # filter warnings, load the configuration and initialize the Dropbox
 # client
 warnings.filterwarnings("ignore")
-conf = json.load(open(args["conf"]))
+conf = json.load(open("conf.json"))
 client = None
 
 vs = VideoStream(src=0).start()
@@ -73,62 +88,62 @@ while True:
     # text
     original_frame = vs.read()
     # timestamp = datetime.datetime.now()
-#    text = "Unoccupied"
+    #    text = "Unoccupied"
 
     # resize the frame, convert it to grayscale, and blur it
     frame = imutils.resize(original_frame, width=500)
 
-#    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#    gray = cv2.GaussianBlur(gray, (21, 21), 0)
-#
-#    # if the average frame is None, initialize it
-#    if avg is None:
-#        print("[INFO] starting background model...")
-#        avg = gray.copy().astype("float")
-#        continue
-#
-#    # accumulate the weighted average between the current frame and
-#    # previous frames, then compute the difference between the current
-#    # frame and running average
-#    cv2.accumulateWeighted(gray, avg, 0.5)
-#    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-#
-#    # threshold the delta image, dilate the thresholded image to fill
-#    # in holes, then find contours on thresholded image
-#    thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
-#    thresh = cv2.dilate(thresh, None, iterations=2)
-#    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#    cnts = imutils.grab_contours(cnts)
+    #    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    #
+    #    # if the average frame is None, initialize it
+    #    if avg is None:
+    #        print("[INFO] starting background model...")
+    #        avg = gray.copy().astype("float")
+    #        continue
+    #
+    #    # accumulate the weighted average between the current frame and
+    #    # previous frames, then compute the difference between the current
+    #    # frame and running average
+    #    cv2.accumulateWeighted(gray, avg, 0.5)
+    #    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+    #
+    #    # threshold the delta image, dilate the thresholded image to fill
+    #    # in holes, then find contours on thresholded image
+    #    thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
+    #    thresh = cv2.dilate(thresh, None, iterations=2)
+    #    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #    cnts = imutils.grab_contours(cnts)
 
     # loop over the contours
-#    for c in cnts:
-#        # if the contour is too small, ignore it
-#        if cv2.contourArea(c) < conf["min_area"]:
-#            continue
-#
-#        #movement_detected = True
-#        #last_movement = datetime.datetime.now()
-#
-#        # compute the bounding box for the contour, draw it on the frame,
-#        # and update the text
-#        (x, y, w, h) = cv2.boundingRect(c)
-#        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-#        text = "Occupied"
-#
-#    cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    #    for c in cnts:
+    #        # if the contour is too small, ignore it
+    #        if cv2.contourArea(c) < conf["min_area"]:
+    #            continue
+    #
+    #        #movement_detected = True
+    #        #last_movement = datetime.datetime.now()
+    #
+    #        # compute the bounding box for the contour, draw it on the frame,
+    #        # and update the text
+    #        (x, y, w, h) = cv2.boundingRect(c)
+    #        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    #        text = "Occupied"
+    #
+    #    cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
     # check to see if the frames should be displayed to screen
     if conf["show_video"]:
         # display the security feed
         # show the frame and record if the user presses a key
 
-#        cv2.imshow("Thresh", thresh)
-#        cv2.imshow("Frame Delta", frameDelta)
-        #cv2.imshow("Image", frame)
+        #        cv2.imshow("Thresh", thresh)
+        #        cv2.imshow("Frame Delta", frameDelta)
+        # cv2.imshow("Image", frame)
         key = cv2.waitKey(1) & 0xFF
 
         # if the `q` key is pressed, break from the lop
-        #if key == ord("q"):
+        # if key == ord("q"):
         while True:
             data = parse_serial(arduino.readline())
             if not data:
@@ -149,10 +164,9 @@ while True:
             cv2.imshow("Image", horizontal)
             cv2.waitKey(500)
             break
-            #image = create_image(data, 255, 255, 1000)
-            #im = Image.fromarray(image)
-            #im = im.convert("L")
-            #im.save("spectro.jpeg")
-            #img = cv2.imread("spectro.jpeg")
-            #cv2.imshow("Image", img)
-
+            # image = create_image(data, 255, 255, 1000)
+            # im = Image.fromarray(image)
+            # im = im.convert("L")
+            # im.save("spectro.jpeg")
+            # img = cv2.imread("spectro.jpeg")
+            # cv2.imshow("Image", img)
